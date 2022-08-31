987ef062fe4496ec23d32b8ca8b747961c1da8a2
翁松青 <1310207176@qq.com>
2022-08-31 18:46:21 +0800

    引入全局样式

--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+//默认全局样式
+import './assets/css/base.css'
 
 createApp(App).use(store).use(router).mount('#app')
